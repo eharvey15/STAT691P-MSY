77a88b73bbf7d522add8c8075b202f8c42e0c0ab
AnnaZhouxy <145226854+AnnaZhouxy@users.noreply.github.com>
2023-09-29 16:16:36 -0400

    Add files via upload

--- logisticROCflights.f23.R
+++ logisticROCflights.f23.R
@@ -4,12 +4,6 @@
 
 
 
-
-
-
-
-
-
 # remove all saved values from R
 
 rm(list=ls())
@@ -53,17 +47,17 @@ set.seed(134562)
 
 # First, create data types
 
-flight.data.types <- c('factor',   # Month
-                        'factor',    # Day of Week 
-                        'factor',    # Carrier 
-                        'factor',    # origin airport
-                        'factor',    # origin state
-                        'factor',    # destination airport
-                        'factor',    # destination state
-                        'numeric',    # Departure Time
-                        'numeric', #  on time or not 
-                        'numeric',    # elapsed time (duration)
-                        'numeric'    # distance
+flight.data.types <- c('month',   # Month
+                       'day',    # Day of Week 
+                       'carrier',    # Carrier 
+                       'origin',    # origin airport
+                       'orstate',    # origin state
+                       'dest',    # destination airport
+                       'destate',    # destination state
+                       'depart',    # Departure Time
+                       'delay', #  on time or not 
+                       'duration',    # elapsed time (duration)
+                       'distance'    # distance
 )
 
 missing.values <- c("NA","")
@@ -80,8 +74,7 @@ missing.values <- c("NA","")
 ##### NEED TO CHANGE FILE NAME FOR YOUR DATA SET #####
 
 
-flight.data <- read.csv("c:/temp/flights2022.f23.csv",
-colClasses=flight.data.types,na.strings=missing.values)
+flight.data <- read.csv("~/Desktop/Fall 2023/Stats 691P/NewOrlFlights2022.csv")
 
 
 # copy the data set to a new name, so that the original data set
@@ -96,7 +89,7 @@ flight.data.new <- flight.data
 ##### CAN CHANGE FILE NAME FOR YOUR DATA SET #####
 
 
-pdf("c:/temp/flightlogistic.f23.pdf")
+
 
 
 # randomly shuffle the data set for extracting
