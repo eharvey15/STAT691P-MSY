647b3c00afc47831e1bbb6d69cf89bb5de23c6b2
first-and-thirty <88009804+first-and-thirty@users.noreply.github.com>
2023-10-03 22:19:21 -0400

    Add Model 3, fix month names, and structure outline

--- logisticstepwiseflights.f23.R
+++ logisticstepwiseflights.f23.R
@@ -1,4 +1,8 @@
-# remove all saved values from R
+--------------------------
+# DATA INITIALIZATION ----
+--------------------------
+
+## Remove all saved values from R and load libraries ----
 
 rm(list=ls())
 
@@ -27,7 +31,7 @@ library("DAAG")
 library("pROC")
 
 
-### Set seed, so that values can be re-created later
+## Set seed, so that values can be re-created later ----
 
 set.seed(234865)
 
@@ -36,7 +40,7 @@ set.seed(234865)
 # U.S. flights originating at a particular airport
 # in the year 2022
 
-# Read in data
+## Read in data ----
 
 # First, create data types
 
@@ -61,11 +65,7 @@ missing.values <- c("NA","")
 # For example: 300 = 5:00 a.m., since 300/60 = 5 hours starting at 12:01 a.m.
 
 
-# The following reads in the data.  
-##### NEED TO CHANGE SUBDIRECTORY FOR DATA LOCATION #####
-
-##### NEED TO CHANGE FILE NAME FOR YOUR DATA SET #####
-
+# The following reads in the data.
 
 flight.data <- read.csv(
   "./NewOrlFlights2022.csv",
@@ -81,12 +81,17 @@ flight.data <- read.csv(
 flight.data.new <- flight.data
 
 
+## Relabel month and day factors ----
+
 # Relabel months from indices to names
 # Not all months are present, so levels are determined from data
 months.names <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun",
                  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
 flight.data.new$month <- factor(
-  months.names[flight.data.new$month],
+  months.names[
+    as.numeric(flight.data.new$month) +
+      min(as.numeric(levels(flight.data.new$month))) - 1
+    ],
   levels = months.names[sort(as.numeric(levels(flight.data.new$month)))]
 )
 
@@ -97,19 +102,16 @@ flight.data.new$day <- factor(
   levels = days.names
 )
 
+----------------------------------------------
+# MODEL TRAINING / TEST DATA ORGANIZATION ----
+----------------------------------------------
 
-# create PDF file for output
-##### NEED TO CHANGE SUBDIRECTORY FOR FILE LOCATION #####
-##### CAN CHANGE FILE NAME FOR YOUR DATA SET #####
-
+## Create PDF file for output ----
 
 pdf("./MSYflightlogistic.f23.pdf")
 
 
-
-# randomly shuffle the data set for extracting
-# train and test data
-
+## Randomly shuffle the data set for extracting train and test data ----
 
 rand.shuffle <- sample(nrow(flight.data.new))
 
@@ -117,11 +119,11 @@ flight.data.new2.new <- flight.data.new[rand.shuffle,]
 
 
 
-## create a new final data set
+## create a new final data set ----
 
 flight.data.fin <- flight.data.new2.new
 
-## split training data into a training batch and a testing batch
+## Split training data into a training batch and a testing batch ----
 ## use 80% for training, 20% for testing later
 
 train.percent <- 0.8
@@ -141,10 +143,12 @@ test.batch <- flight.data.fin[-training.rows, ]
 
 
 
-###########################
-#### START STEPWISE HERE
-###########################
+--------------------------
+# START STEPWISE HERE ----
+--------------------------
 
+## Model 1 ----
+  
 # start with a model that includes the 5 predictors, and all
 # pairwise interaction terms
 
@@ -154,9 +158,9 @@ test.batch <- flight.data.fin[-training.rows, ]
 # We will use backward stepwise regression, which starts with all predictors
 # in the model and removes predictors that do not meet a threshold
 
-
-
-flight.logistic.step1.pre <- glm(delay ~ (day + depart + duration + month)^2 + carrier,data=train.batch,family=binomial("logit"))
+flight.logistic.step1.pre <- glm(delay ~ (day + depart + duration + month)^2 + carrier,
+                                 data=train.batch,
+                                 family=binomial("logit"))
 
 # "test="F"" is to test using the F test statistic
 
@@ -165,13 +169,19 @@ flight.logistic.step1 <- step(flight.logistic.step1.pre, direction="backward", t
 summary(flight.logistic.step1)
 
 
+## Model 2 ----
+
 #### use polynomials
 
 # "poly(depart,degree=3,raw=TRUE)[,:3]" uses only the second and third
 # column, which are: depart^2, depart^3 
 
 
-flight.logistic.step2.pre <- glm(delay ~ (day + depart + duration + month)^2 + carrier + poly(depart, degree=3,raw=TRUE)[,2:3]+ poly(duration,degree=3,raw=TRUE)[,2:3],data=train.batch,family=binomial("logit"))
+flight.logistic.step2.pre <- glm(delay ~ (day + depart + duration + month)^2 + carrier +
+                                          poly(depart, degree=3, raw=TRUE)[,2:3] +
+                                          poly(duration, degree=3, raw=TRUE)[,2:3],
+                                 data=train.batch,
+                                 family=binomial("logit"))
 
 
 # "test="F"" is to test using the F test statistic
@@ -181,10 +191,30 @@ flight.logistic.step2 <- step(flight.logistic.step2.pre, direction="backward", t
 summary(flight.logistic.step2)
 
 
+## Model 3 ----
 
-############################
-###### MODEL EVALUATION
-############################
+#### Use three-term interactions and lower terms for day, depart, duration, and month
+
+# Add two-term main effect of destination airport with carrier
+# Most of these terms should hopefully drop out due to backward stepwise regression
+#  leaving most significant carrier destinations
+#  (which would ideally align with higher volume carrier destinations from MSY)
+
+flight.logistic.step3.pre <- glm(delay ~ (day + depart + duration + month)^3 + (carrier + dest)^2,
+                                 data=train.batch,
+                                 family=binomial("logit"))
+
+# "test="F"" is to test using the F test statistic
+
+flight.logistic.step3 <- step(flight.logistic.step3.pre, direction="backward", test="F")
+
+summary(flight.logistic.step3)
+
+
+
+-----------------------
+# MODEL EVALUATION ----
+-----------------------
 
 ## We will check the performance of our model, using the
 ## "test.batch" data (the remaining 20% of the data)
@@ -192,7 +222,7 @@ summary(flight.logistic.step2)
 ## ROC CURVE AND AUC HERE
 
 
-## Model 1 here
+## Model 1 ----
 
 # use the logistic regression model from "flight.logistic.1"
 # and apply it to the "test.batch" data
@@ -208,18 +238,10 @@ as.numeric(test_roc1$auc)
 
 # plot the ROC curve and show AUC on curve
 
-# NEED TO EDIT THE TITLE FOR WHICH MODEL
-
-plot(test_roc1,main="ROC Curve for \n Stepwise Logistic Regression 1",print.auc=TRUE)
+plot(test_roc1, main="ROC Curve for \n Stepwise Logistic Regression 1", print.auc=TRUE)
 
 
-## Model 2 here
-
-
-## We will check the performance of our model, using the
-## "test.batch" data (the remaining 20% of the data)
-
-## ROC CURVE AND AUC HERE
+## Model 2 ----
 
 
 # use the logistic regression model from "flight.logistic.step2"
@@ -236,12 +258,29 @@ as.numeric(test_roc2$auc)
 
 # plot the ROC curve and show AUC on curve
 
-# NEED TO EDIT THE TITLE FOR WHICH MODEL
+plot(test_roc2, main="ROC Curve for \n Stepwise Logistic Regression 2", print.auc=TRUE)
 
 
-plot(test_roc2,main="ROC Curve for \n Stepwise Logistic Regression 2",print.auc=TRUE)
+## Model 3 ----
 
+# use the logistic regression model from "flight.logistic.1"
+# and apply it to the "test.batch" data
+
+# create probabilities for the "test.batch" data:
+
+test_prob3 <- predict(flight.logistic.step3, newdata = test.batch, type = "response")
+# create ROC curve information and AUC
+
+test_roc3 <- roc(test.batch$delay ~ test_prob3, plot = FALSE, print.auc = TRUE)
+
+as.numeric(test_roc3$auc)
+
+# plot the ROC curve and show AUC on curve
+
+plot(test_roc3, main="ROC Curve for \n Stepwise Logistic Regression 3", print.auc=TRUE)
+
+
+## Close PDF connection ----
 
 graphics.off()
-
 
