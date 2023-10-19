5bbd0a11e4ea79e50880c0c7e5edc525c4e67dd2
first-and-thirty <88009804+first-and-thirty@users.noreply.github.com>
2023-10-18 21:54:41 -0400

    Formatting before model changes

--- simulatelogisticf23.R
+++ simulatelogisticf23.R
@@ -1,11 +1,7 @@
 
-
-
-
-
-
-
-
+------------------------
+# ENVIRONMENT SETUP ----
+------------------------
 
 ## simulate logistic regression data
 
@@ -26,11 +22,11 @@ library("corrgram")
 
 library("Hmisc")
 
-library(caret)
+library("caret")
 
-library(pscl)
+library("pscl")
 
-library(glmnet)
+library("glmnet")
 
 library("car")
 
@@ -41,7 +37,9 @@ library("DAAG")
 library("pROC")
 
 
-
+------------------------------------------
+# DATA SIMULATION VARIABLES (MODEL 1) ----  
+------------------------------------------
 
 # number of data values to simulate 
 
@@ -51,17 +49,16 @@ set.seed(874695)
 
 # p is probability
 
-p <- rep(NA,N)
+p <- rep(NA, N)
 
 # x's are continuous covariates
 
-x1 <- rep(NA,N)
-x2 <- rep(NA,N)
-x3 <- rep(NA,N)
-x4 <- rep(NA,N)
-x5 <- rep(NA,N)
-x6 <- rep(NA,N)
-
+x1 <- rep(NA, N)
+x2 <- rep(NA, N)
+x3 <- rep(NA, N)
+x4 <- rep(NA, N)
+x5 <- rep(NA, N)
+x6 <- rep(NA, N)
 
 # simulate correlated data
 
@@ -69,7 +66,7 @@ num.corr.predictors <- 3  # number of correlated predictors
 
 # this can be changed to have more predictors correlated than 3
 
-x <- matrix(NA,N,num.corr.predictors)
+x <- matrix(NA, N, num.corr.predictors)
 
 # next, specify the variance-covariance matrix
 
@@ -78,7 +75,7 @@ var.diagonal <- 1  # this is variance on diagonal
 ## next, specify covariance of "0.2" for predictors x1,x2,x3
 ## this can be increased or decreased
 
-cov.off.diagonal <- 0.2*var.diagonal  # covariance of predictors
+cov.off.diagonal <- 0.2 * var.diagonal  # covariance of predictors
 # this is the value off the diagonal in the covariance matrix
 
 # "cov.off.diagonal" value has restrictions for the covariance matrix
@@ -87,23 +84,23 @@ cov.off.diagonal <- 0.2*var.diagonal  # covariance of predictors
 
 # the following sets the means to zero
 
-mean.vec <- rep(0,num.corr.predictors)
+mean.vec <- rep(0, num.corr.predictors)
 
 # the following assigns the covariance matrix
 
-sigma.mat <- matrix(cov.off.diagonal,num.corr.predictors,num.corr.predictors)
+sigma.mat <- matrix(cov.off.diagonal, num.corr.predictors, num.corr.predictors)
 
 # the following assigns the diagonal of the covariance matrix
 
 for (i in 1:num.corr.predictors){
-sigma.mat[i,i] <- var.diagonal
+  sigma.mat[i,i] <- var.diagonal
 }
 
 
 # create matrix of 3 correlated predictors
 # this can be changed to have more than 3 predictors that are correlated
 
-x <- round(rmvnorm(N,mean=mean.vec, sigma=sigma.mat ),6)
+x <- round(rmvnorm(N, mean=mean.vec, sigma=sigma.mat), 6)
          
 x1 <- x[,1]
 x2 <- x[,2]
@@ -111,9 +108,9 @@ x3 <- x[,3]
 
 # create independent normally distributed predictors
 
-x4 <- round(rnorm(N,mean=2,sd=1),6)
+x4 <- round(rnorm(N, mean=2, sd=1), 6)
 
-x5 <- round(rnorm(N,mean=-2,sd=1),6)
+x5 <- round(rnorm(N, mean=-2, sd=1), 6)
 
 # create a binary predictor; can change prob. of success
 # of "0.3" to higher or lower
@@ -123,13 +120,17 @@ x6 <- rbinom(N, 1, prob=0.3)
 
 #### create new variables based on the first 6 predictors
 
-x7 <- x1*x4
-x8 <- x2*x4*x5   # third-order variable
-x9 <- x1^2       # second-degree polynomial
-x10 <- x3*x2
-x11 <- (x2)^3    # third-degree polynomial
-x12 <- x1*x2*x6
+x7 <- x1 * x4
+x8 <- x2 * x4 * x5    # third-order variable
+x9 <- (x1) ^ 2        # second-degree polynomial
+x10 <- x3 * x2
+x11 <- (x2) ^ 3       # third-degree polynomial
+x12 <- x1 * x2 * x6
 
+
+---------------------------------------------
+# DATA SIMULATION COEFFICIENTS (MODEL 1) ----
+---------------------------------------------
 
 # assign values for beta values (regression coefficients)
 
@@ -149,45 +150,53 @@ b11 <- -1.4
 b12 <- 0.12
 
 
+--------------------------------
+# DATA SIMULATION (MODEL 1) ----
+--------------------------------
 
 # simulate probabilities based on logistic regression model
 
 for (i in 1:N){
-p[i] <- exp( b1*x1[i] + b2*x2[i] + b3*x3[i] + b4*x4[i] + b5*x5[i] + b6*x6[i] +
-b7*x7[i] + b8*x8[i] + b9*x9[i] + b10*x10[i] + b11*x11[i] + b12*x12[i] )/(1+
-exp( b1*x1[i] + b2*x2[i] + b3*x3[i] + b4*x4[i] + b5*x5[i]+ b6*x6[i] +
-b7*x7[i] + b8*x8[i] + b9*x9[i] + b10*x10[i] + b11*x11[i] + b12*x12[i] ))
+  xterm = b1 * x1[i] + b2 * x2[i] + b3 * x3[i] + b4 * x4[i] + b5 * x5[i] + b6 * x6[i] +
+    b7 * x7[i] + b8 * x8[i] + b9 * x9[i] + b10 * x10[i] + b11 * x11[i] + b12 * x12[i]
+  
+  p[i] <- exp(xterm) / (1 + exp(xterm))
 }
 
 
 # simulate binary "y" values based on probabilities
 
-y <- rep(NA,N)
+y <- rep(NA, N)
 
 for (i in 1:N){
-y[i] <- rbinom(1,1,p[i])
+  y[i] <- rbinom(1, 1, p[i])
 }
 
 
 # check the values of the logistic regression model, and
 # whether they are significant at alpha = 0.1 or less
 
-logistic.out <- glm(y ~ x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10 + x11 + x12 -1, family=binomial(logit))
+logistic.out <- glm(y ~ x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10 + x11 + x12 - 1, family=binomial("logit"))
 
 
 # compare the coefficients here with the values that were simulated
 
-summary(logistic.out)
+(logistic.out.summ = summary(logistic.out))
 
 # check how many of the predictors have p-values
-# less than an alpha-level cutoff (such as 0.1 or 0.25).
+# less than or equal to an alpha-level cutoff (such as 0.1 or 0.25).
+
+stopifnot(
+  sum(logistic.out.summ$coefficients[1:6,4] <= 0.1) >= 4, # At least 4 of 6 main predictors
+  sum(logistic.out.summ$coefficients[,4] <= 0.1) >= 10    # At least 10 total predictors
+)
 
 # write out the simulation data
 
 rownum <- rep(1:length(y))
 
 
-sim.data.out <- cbind(rownum,y,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12)
+sim.data.out <- cbind(rownum, y, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12)
 
 
 ## split simulation data into a training batch and a testing batch
@@ -206,23 +215,23 @@ test.batch <- data.frame(sim.data.fin[-training.rows, ])
 
 
 
-################################################
-#### CHECK MODEL PERFORMANCE HERE USING LOGISTIC
-################################################
+--------------------------------
+# CHECK MODEL 1 PERFORMANCE ----
+--------------------------------
 
 
 # start with the training batch to create model
 
 
-fit.logistic.1 <- glm(y ~ x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10 + x11 + x12 -1,data=train.batch, family=binomial(logit))
+fit.logistic.1 <- glm(y ~ x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10 + x11 + x12 - 1, data=train.batch, family=binomial("logit"))
 
 
 summary(fit.logistic.1)
 
 
-############################
-###### MODEL EVALUATION
-############################
+-------------------------
+# MODEL 1 EVALUATION ----
+-------------------------
 
 
 # use logistic regression model from "fit.logistic.1"
@@ -239,9 +248,7 @@ as.numeric(test_roc1$auc)
 
 # plot the ROC curve and show AUC on curve
 
-# NEED TO EDIT THE TITLE FOR WHICH MODEL
-
-plot(test_roc1,main="ROC Curve for Test Batch \n Logistic Regression 1",print.auc=TRUE)
+plot(test_roc1, main="ROC Curve for Test Batch \n Logistic Regression 1", print.auc=TRUE)
 
 
 
@@ -260,9 +267,15 @@ plot(test_roc1,main="ROC Curve for Test Batch \n Logistic Regression 1",print.au
 #### For high AUC data
 
 options(scipen=10)
-write.table(sim.data.out,
-"c:/a1projcourse/data/HighAUC.logistic.all.txt",sep="\t",
-row.names=F,col.names=T)
+
+write.table(
+  sim.data.out,
+  "./data/HighAUC.logistic.all.txt",
+  sep="\t",
+  row.names=F,
+  col.names=T
+)
+
 options(scipen=0)
 
 
