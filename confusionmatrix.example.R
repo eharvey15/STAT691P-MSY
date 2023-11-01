7046989142976eda23b9afc3c581a00523012b22
first-and-thirty <88009804+first-and-thirty@users.noreply.github.com>
2023-10-31 20:36:09 -0400

    Modify for initial high/low AUC confusion matrices

--- confusionmatrix.example.R
+++ confusionmatrix.example.R
@@ -1,30 +1,66 @@
 
 
-
-
-
-
-
-
-
-
-
-
-# remove all saved values from R
+# Remove all saved values from R
 
 rm(list=ls())
 
-
+# Import necessary libraries
 
 library(caret)
 
 
+########################
+# HIGH AUC ACCURACY ----
+########################
 
-predict.x1 <- as.factor(c(1,0,1,0,1,0,0,1))
+# Specify data types for imports
+highAUC.types <- c(
+    "numeric",  # rownum
+    "factor"    # y (predicted response)
+)
 
-truth.x2 <- as.factor(c(1,1,0,0,1,0,1,1))
+# Import simulated data from High AUC model
+highAUC.predict <- read.table(
+    "./data/predict.HighAUC.testdata.txt",
+    colClasses = highAUC.types,
+    header = TRUE
+)
 
-outcome1.test <- confusionMatrix(predict.x1,truth.x2)
+# Import true data for High AUC
+highAUC.true <- read.table(
+    "./true.y.HighAUC.testing.txt",
+    colClasses = highAUC.types,
+    header = TRUE
+)
+
+# Compare predictions to true response values
+(outcome1.test <- confusionMatrix(highAUC.predict$predicty, highAUC.true$y))
 
 
 
+#######################
+# LOW AUC ACCURACY ----
+#######################
+
+# Specify data types for imports
+lowAUC.types <- c(
+    "numeric",  # rownum
+    "factor"    # y (predicted response)
+)
+
+# Import simulated data from Low AUC model
+lowAUC.predict <- read.table(
+    "./data/predict.LowAUC.testdata.txt",
+    colClasses = lowAUC.types,
+    header = TRUE
+)
+
+# Import true data for Low AUC
+lowAUC.true <- read.table(
+    "./true.y.LowAUC.testing.txt",
+    colClasses = lowAUC.types,
+    header = TRUE
+)
+
+# Compare predictions to true response values
+(outcome2.test <- confusionMatrix(lowAUC.predict$predicty, lowAUC.true$y))
